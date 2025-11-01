1e30761ea0730580c78840b10bc102fd2527db7b
Dano <danocolombo@gmail.com>
2025-11-01 08:49:30 -0400

    added latest aws amplify

--- aws-exports.js
+++
@@ -1,7 +0,0 @@
-// This file was intentionally cleared by an automated script.
-// The original root-level `aws-exports.js` was a duplicate/stale generated file.
-// It has been replaced with a harmless stub to avoid accidental usage.
-
-const awsmobile = {};
-
-export default awsmobile;
